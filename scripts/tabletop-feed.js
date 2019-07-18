e5d63a1cfb6b9068b7f5e57b7d1f1b6289039655
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-07-18 01:54:09 -0400

    Updating to live site

--- scripts/tabletop-feed.js
+++ scripts/tabletop-feed.js
@@ -64,7 +64,7 @@ function writeTableWith(dataSource) {
 
     var oTable = jqueryNoConflict("#data-table-container").dataTable({
         "sPaginationType": "bootstrap",
-        "iDisplayLength": 96,
+        "iDisplayLength": 111,
         "aaData": dataSource,
         "aoColumns": createTableColumns(),
         "processing": true,
